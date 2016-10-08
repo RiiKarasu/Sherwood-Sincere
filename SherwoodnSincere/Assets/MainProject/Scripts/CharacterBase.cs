566c8acb7ea128ea23079afe6adbcd16dc98a415
任占胜 <1036025294@qq.com>
2016-10-08 16:46:35 +0800

    no message

--- SherwoodnSincere/Assets/MainProject/Scripts/CharacterBase.cs
+++ SherwoodnSincere/Assets/MainProject/Scripts/CharacterBase.cs
@@ -13,19 +13,9 @@
 using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
+
 public class CharacterBase : MonoBehaviour
 {
-
-	enum Race
-	{
-		Bunny,
-		Warthog
-	}
-=======
-
-public class CharacterBase : MonoBehaviour {			
->>>>>>> 39cb34362aadab3e8438456cb0a8930afa90013f
 
 	#region 角色属性字段
 
@@ -38,12 +28,8 @@ public class CharacterBase : MonoBehaviour {
 	/// <summary>
 	/// 角色种族
 	/// </summary>
-<<<<<<< HEAD
-	Race race;
-
-=======
 	string race;
->>>>>>> 39cb34362aadab3e8438456cb0a8930afa90013f
+
 	/// <summary>
 	/// 角色血量
 	/// </summary>
@@ -103,15 +89,22 @@ public class CharacterBase : MonoBehaviour {
 	/// </summary>
 	public float roSpeed = 180f;
 
-<<<<<<< HEAD
+
+
 	#endregion
 
 	#region 构造方法
 
+	/// <summary>
+	/// 实例化角色方法
+	/// </summary>
+	/// <param name="id">Identifier.</param>
+	/// <param name="ant">Ant.</param>
+	/// <param name="aud">Aud.</param>
 	public CharacterBase (int id, Animator ant, AudioSource aud)
 	{
 		name = ShareDataBase.sDb.SelectFieldSql ("SELECT name FROM CharactorBase WHERE id=" + id).ToString ();
-		//		race = ShareDataBase.sDb.SelectFieldSql ("SELECT race FROM CharactorBase WHERE id =" + id).ToString;
+		race = ShareDataBase.sDb.SelectFieldSql ("SELECT race FROM CharactorBase WHERE id =" + id).ToString ();
 		hp = float.Parse (ShareDataBase.sDb.SelectFieldSql ("SELECT hp FROM CharactorBase WHERE id =" + id).ToString ());
 		attack = float.Parse (ShareDataBase.sDb.SelectFieldSql ("SELECT attack FROM CharactorBase WHERE id=" + id).ToString ());
 		attackOfAera = float.Parse (ShareDataBase.sDb.SelectFieldSql ("SELECT attackOfAera FROM CharactorBase WHERE id=" + id).ToString ());
@@ -120,46 +113,25 @@ public class CharacterBase : MonoBehaviour {
 		this.ant = ant;
 		this.aud = aud;
 	}
-=======
 
-
-	#endregion
-
-	#region 构造方法
-	/// <summary>
-	/// 实例化角色方法
-	/// </summary>
-	/// <param name="id">Identifier.</param>
-	/// <param name="ant">Ant.</param>
-	/// <param name="aud">Aud.</param>
-	public CharacterBase(int id,Animator ant,AudioSource aud){
-		name = ShareDataBase.sDb.SelectFieldSql ("SELECT name FROM CharactorBase WHERE id="+id).ToString();
-		race = ShareDataBase.sDb.SelectFieldSql ("SELECT race FROM CharactorBase WHERE id =" + id).ToString();
-		hp = float.Parse(ShareDataBase.sDb.SelectFieldSql("SELECT hp FROM CharactorBase WHERE id ="+id).ToString());
-		attack = float.Parse (ShareDataBase.sDb.SelectFieldSql("SELECT attack FROM CharactorBase WHERE id="+id).ToString());
-		attackOfAera = float.Parse (ShareDataBase.sDb.SelectFieldSql("SELECT attackOfAera FROM CharactorBase WHERE id="+id).ToString());
-		attackSpeed = float.Parse (ShareDataBase.sDb.SelectFieldSql("SELECT attackSpeed FROM CharactorBase WHERE id="+id).ToString());
-		moveSpeed = float.Parse (ShareDataBase.sDb.SelectFieldSql("SELECT moveSpeed FROM CharactorBase WHERE id="+id).ToString());
-		this.ant = ant;
-		this.aud = aud;
-	}
 	/// <summary>
 	/// 获取角色信息方法
 	/// </summary>
 	/// <param name="id">Identifier.</param>
-	public CharacterBase(int id){
-		name = ShareDataBase.sDb.SelectFieldSql ("SELECT name FROM CharactorBase WHERE id="+id).ToString();
-		hp = float.Parse(ShareDataBase.sDb.SelectFieldSql("SELECT hp FROM CharactorBase WHERE id ="+id).ToString());
-		attack = float.Parse (ShareDataBase.sDb.SelectFieldSql("SELECT attack FROM CharactorBase WHERE id="+id).ToString());
-		attackOfAera = float.Parse (ShareDataBase.sDb.SelectFieldSql("SELECT attackOfAera FROM CharactorBase WHERE id="+id).ToString());
-		attackSpeed = float.Parse (ShareDataBase.sDb.SelectFieldSql("SELECT attackSpeed FROM CharactorBase WHERE id="+id).ToString());
-		moveSpeed = float.Parse (ShareDataBase.sDb.SelectFieldSql("SELECT moveSpeed FROM CharactorBase WHERE id="+id).ToString());
+	public CharacterBase (int id)
+	{
+		name = ShareDataBase.sDb.SelectFieldSql ("SELECT name FROM CharactorBase WHERE id=" + id).ToString ();
+		hp = float.Parse (ShareDataBase.sDb.SelectFieldSql ("SELECT hp FROM CharactorBase WHERE id =" + id).ToString ());
+		attack = float.Parse (ShareDataBase.sDb.SelectFieldSql ("SELECT attack FROM CharactorBase WHERE id=" + id).ToString ());
+		attackOfAera = float.Parse (ShareDataBase.sDb.SelectFieldSql ("SELECT attackOfAera FROM CharactorBase WHERE id=" + id).ToString ());
+		attackSpeed = float.Parse (ShareDataBase.sDb.SelectFieldSql ("SELECT attackSpeed FROM CharactorBase WHERE id=" + id).ToString ());
+		moveSpeed = float.Parse (ShareDataBase.sDb.SelectFieldSql ("SELECT moveSpeed FROM CharactorBase WHERE id=" + id).ToString ());
 	}
->>>>>>> 39cb34362aadab3e8438456cb0a8930afa90013f
 
 	#endregion
 
 	#region 公共方法
+
 	/// <summary>
 	/// 角色受到攻击
 	/// </summary>
@@ -169,22 +141,17 @@ public class CharacterBase : MonoBehaviour {
 		hp -= damage;
 		ant.SetTrigger (HashID.UnderAttack);
 	}
-<<<<<<< HEAD
 
-	public void PlayerDead (AudioClip DeathAudio)
-	{
-		ant.SetBool (HashID.PlayerDeath, true);
-		AudioSource.PlayClipAtPoint (DeathAudio, transform.position);
-=======
 	/// <summary>
 	/// 角色死亡方法
 	/// </summary>
 	/// <param name="DeathAudio">Death audio.</param>
-	public void PlayerDead(AudioClip DeathAudio){
-		ant.SetBool (HashID.PlayerDeath,true);
-		AudioSource.PlayClipAtPoint (DeathAudio,transform.position);
->>>>>>> 39cb34362aadab3e8438456cb0a8930afa90013f
+	public void PlayerDead (AudioClip DeathAudio)
+	{
+		ant.SetBool (HashID.PlayerDeath, true);
+		AudioSource.PlayClipAtPoint (DeathAudio, transform.position);
 	}
+
 	/// <summary>
 	///  受技能效果影响
 	/// </summary>
@@ -192,13 +159,14 @@ public class CharacterBase : MonoBehaviour {
 	/// <param name="asEffect">攻速减少.</param>
 	/// <param name="lastDamage">持续伤害.</param>
 	/// <param name="time">效果持续时间.</param>
-	public void EffectTimeCast(float msEffect,float asEffect,float lastDamage,float time){
-		timer+= Time.deltaTime;
+	public void EffectTimeCast (float msEffect, float asEffect, float lastDamage, float time)
+	{
+		timer += Time.deltaTime;
 		if (timer < time) {
-			curAttackSpeed =attackSpeed- asEffect;
-			curMoveSpeed =moveSpeed- msEffect;
+			curAttackSpeed = attackSpeed - asEffect;
+			curMoveSpeed = moveSpeed - msEffect;
 			hp -= lastDamage;
-			EffectTimeCast (msEffect,asEffect,lastDamage,time);
+			EffectTimeCast (msEffect, asEffect, lastDamage, time);
 		} else {
 			curMoveSpeed = moveSpeed;
 			curAttackSpeed = attackSpeed;
